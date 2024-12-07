512e04948247919f4b2c90cd4952ac6b43610643
Dima Kiselev <121449750+oonixxxxx@users.noreply.github.com>
2024-12-07 14:01:24 +0000

    refactor code

--- app/other/buttons.py
+++ app/other/buttons.py
@@ -1,41 +1,8 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+
 class Buttons: 
     menu_button = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Корзина 🛒')],
                                         [KeyboardButton(text='Информация о нас 📃✏️'), KeyboardButton(text='Контакт 📰')]],
             resize_keyboard=True,
             input_field_placeholder='Выберите пунт меню.')
-
-
-    busket_button = ReplyKeyboardMarkup(keyboard=[
-        [KeyboardButton(text='В меню🧑🏿‍💻🗂')],
-        [KeyboardButton(text='Добавить товар🛍'), KeyboardButton(text='Перейти к оплате💰💳')]],
-        resize_keyboard=True)
-
-
-    add_item_button = ReplyKeyboardMarkup(keyboard=[
-        [KeyboardButton(text='Добавить еще товар🛍'), KeyboardButton(text='Перейти к оплате💰💳')]], 
-        resize_keyboard=True,
-        input_field_placeholder='Введите артикуль')
-
-
-    verifcation = ReplyKeyboardMarkup(keyboard=[
-        [KeyboardButton(text='Да, это мои данные✅'), KeyboardButton(text='Нет, я хочу переписать их❌')]],
-        resize_keyboard=True,
-        input_field_placeholder='Выбирите ваш вариант'
-    )
-
-    go_to_pay_button = ReplyKeyboardMarkup(keyboard=[
-        [KeyboardButton(text='/reg'), KeyboardButton(text='В меню🧑🏿‍💻🗂')]],
-        resize_keyboard=True
-    )
-
-    about_us = ReplyKeyboardMarkup(keyboard=[
-        [KeyboardButton(text='В меню🧑🏿‍💻🗂')]],
-        resize_keyboard=True
-    )
-
-    bool_button = ReplyKeyboardMarkup(keyboard=[
-        [KeyboardButton(text='В меню🧑🏿‍💻🗂')]],
-        resize_keyboard=True
-    )
