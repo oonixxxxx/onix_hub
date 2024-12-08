f34c818d2b407ef0d6a6454ee0a2e13c56de8693
Dima Kiselev <121449750+oonixxxxx@users.noreply.github.com>
2024-12-08 11:38:41 +0000

    refactor handlers.py

--- app/handlers.py
+++ app/handlers.py
@@ -12,22 +12,27 @@ router = Router()
 
 promos = Promo.promos
 
+
 @router.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     await message.answer(f"Hello! Я бот длля продажи предметов в локальном магазине \n что бы перейти к выполнению заказа напиши 'Корзина 🛒'", 
                          reply_markup=Buttons.menu_button)
 
+
 @router.message(F.text == 'В меню🧑🏿‍💻🗂')
 async def get_menu(message: Message):
     await message.answer(str(Texts.main_page_text), reply_markup=Buttons.menu_button)
+
 
 @router.message(F.text == 'Конткты')
 async def get_contact(message: Message):
     await message.answer(str(Texts.contact_text), reply_markup=Buttons.contact_button)
 
+
 @router.message(F.text == 'Ввести промокод')
 async def input_promocode(message: Message):
     await message.answer('Введите промокод')
+
 
 @router.message(F.text == 'Получить реферальную ссылку')
 async def get_referral_link(message: Message):
